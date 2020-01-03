82606d871c72bccb8962f50cb374595bcc2ab77d
hbcho <52642437+hbcho@users.noreply.github.com>
2020-01-03 17:59:03 +0900

    Update scanner_ibeacon_example.py

--- examples/scanner_ibeacon_example.py
+++ examples/scanner_ibeacon_example.py
@@ -6,7 +6,7 @@ def callback(bt_addr, rssi, packet, additional_info):
 
 # scan for all iBeacon advertisements from beacons with the specified uuid 
 scanner = BeaconScanner(callback, 
-    device_filter=IBeaconFilter(uuid="e5b9e3a6-27e2-4c36-a257-7698da5fc140")
+    device_filter=IBeaconFilter(uuid="e2c56db5-dffb-48d2-b060-d0f5a71096e0")
 )
 scanner.start()
 time.sleep(10)
